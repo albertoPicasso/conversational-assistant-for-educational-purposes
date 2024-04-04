7af06cffc9a6128437b299584aa9652f2930aacf
albertoPicasso <amadejon1999@gmail.com>
2024-04-04 23:36:10 +0200

    TTS online arreglado(No guardaba en la carpeta del cliente)

--- codigos/TTSFolder/openAITTS.py
+++ codigos/TTSFolder/openAITTS.py
@@ -12,13 +12,13 @@ class OpenAITTS(TtsInterface):
         self.model = model
 
 
-    def speak(self, text: str) -> str:
+    def speak(self, text: str, uid: str) -> str:
 
 
         # Define the output filename
         name = "salida.mp3"  
         namewav = "salida.wav"
-
+        path = os.path.join(os.getcwd(), uid, name)
         # Perform text-to-speech synthesis and save audio
         response = self.client.audio.speech.create(
             model="tts-1",
@@ -27,12 +27,17 @@ class OpenAITTS(TtsInterface):
             response_format="mp3" 
         )  
         
-        response.stream_to_file(name)
+        response.stream_to_file(path)
 
+        #Load .mp3 in memory 
+        audio = AudioSegment.from_mp3(path)
+
+        path = os.path.join(os.getcwd(), uid, namewav)
         #Convert mp3 to wav
-        audio = AudioSegment.from_mp3(name)
-        audio.export(namewav, format="wav")
+        audio.export(path, format="wav")
 
-        return namewav
+        #remove mp2
+
+        return path
     
 
